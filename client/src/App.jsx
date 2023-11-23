48b6d175ba2d5ebc8c1a0a1c3c19d2ff234ce573
Evgeni Markov <evgenimarkov@gmail.com>
2023-11-23 21:27:53 +0200

    Add home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
     return (
         <div id="box">
             <Header />
+            <Home />
         </div>
 
     );

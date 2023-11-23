53e53f3a461f852cdf5d31de5e3331b594ce854e
Evgeni Markov <evgenimarkov@gmail.com>
2023-11-23 23:58:13 +0200

    Add game list page and navigation links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
+import GameList from './components/game-list/GameList.jsx';
+
 
 function App() {
     return (
         <div id="box">
             <Header />
-            <Home />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/games' element={<GameList />} />
+            </Routes>
+
         </div>
 
     );

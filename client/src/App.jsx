356718736001e80572947201669e53070ca340b5
Evgeni Markov <evgenimarkov@gmail.com>
2023-11-24 21:38:45 +0200

    Add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import GameList from './components/game-list/GameList.jsx';
 import GameCreate from './components/game-create/GameCreate.jsx';
+import Login from './components/login/Login.jsx';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/games' element={<GameList />} />
                 <Route path='/games/create' element={<GameCreate />} />
+                <Route path='/login' element={<Login />} />
 
             </Routes>
 

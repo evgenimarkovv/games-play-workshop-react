97b01af67497aed59201a50690903910009edf10
Evgeni Markov <evgenimarkov@gmail.com>
2023-11-25 19:51:23 +0200

    Modify requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,22 +1,15 @@
-import { request } from '../lib/request.js';
+import * as request from '../lib/request.js';
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
     return Object.values(result);
 };
 
 export const create = async (gameData) => {
-    const res = await fetch(`${baseUrl}`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData),
-    });
-    const result = res.json();
+    const result = await request.post(baseUrl, gameData);
 
     return result;
 };

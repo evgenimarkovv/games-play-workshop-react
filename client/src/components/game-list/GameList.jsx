2f4675dfed9a165b2db98cbc8b8fb78f31585ab4
Evgeni Markov <evgenimarkov@gmail.com>
2023-11-25 10:05:00 +0200

    Add get all games service method

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,32 +1,45 @@
+import { useEffect, useState } from 'react';
+import * as gameService from '../../services/gameService.js';
+
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result));
+    }, []);
+
+    console.log(games);
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {/* <!-- Display div: with information about every game (if any) --> */}
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                        <h6>Action</h6>
-                        <h2>Cover Fire</h2>
-                        <a href="#" className="details-button">Details</a>
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Cover Fire</h2>
+                    <a href="#" className="details-button">Details</a>
                 </div>
 
             </div>
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                        <h6>Action</h6>
-                        <h2>Zombie lang</h2>
-                        <a href="#" className="details-button">Details</a>
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <a href="#" className="details-button">Details</a>
                 </div>
 
             </div>
             <div className="allGames">
                 <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                        <h6>Action</h6>
-                        <h2>MineCraft</h2>
-                        <a href="#" className="details-button">Details</a>
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>MineCraft</h2>
+                    <a href="#" className="details-button">Details</a>
                 </div>
             </div>
 
